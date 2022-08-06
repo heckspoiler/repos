2a8dbfbdda083350ee56406b9fb7f78e0b983cc4
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-08-06 21:36:57 +0200

    tests

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,32 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+   return Math.floor(Math.random() * 10)
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+        if(humanDifference <= computerDifference) {
+            return true;
+        } else {
+            return false;
+        }
+}
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore ++;
+    } else {
+        computerScore ++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber ++;
+}
+
+
+
+
