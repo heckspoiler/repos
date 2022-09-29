5643f9bd248f5bb3b11528755b8e4f42163979c3
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-09-29 20:03:22 +0200

    many commits

---
+++ js_for_dummies/1_Projekt/script.js
@@ -0,0 +1,10 @@
+let required = function() {
+    let empt = document.form1.text1.value;
+
+    if(empt === ""){
+        alert('type something!!!!!')
+    } else {
+        alert('well done, bitch');
+        document.body.style.backgroundColor ="blue";
+    }
+}
